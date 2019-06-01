fc31e9e5f96a9d2f46b0a22ce4a7b2fb23019e41
Deka <dekachan16@gmail.com>
2019-06-01 17:39:32 +0900

    config for FontAwesome 5

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,8 +19,10 @@ const nuxtConfig: NuxtConfiguration = {
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       {
         rel: 'stylesheet',
-        href:
-          'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons'
+        href: 'https://use.fontawesome.com/releases/v5.8.2/css/all.css',
+        integrity:
+          'sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay',
+        crossorigin: 'anonymous'
       }
     ]
   },
